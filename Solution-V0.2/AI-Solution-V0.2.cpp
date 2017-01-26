55b1de3fea959701864ce002263b582950c2b122
adriantre <adrianto@stud.ntnu.no>
2017-01-26 09:45:14 +0100

    V0.2
    Done:
    	Velger det beste action langs planka, fra det tidligst mulige kryssningspunktet mellom dronen og roboten som ligger best til.
    
    Todo:
    	Sjekke at kryssningspunktet ikke ligger etter endepunktet til planka, i sjekke bakover langs planka.
    	Fortelle cmd hvilken action som skal gjøres i hvilket punkt, evt bare kjøre tracking?

--- Solution-V0.2/AI-Solution-V0.2.cpp
+++ Solution-V0.2/AI-Solution-V0.2.cpp
@@ -14,8 +14,23 @@ struct Plank
     float y_1;
     float x_2;
     float y_2;
+    float length;
 };
 
+struct IntersectionPoint
+{
+    float x;
+    float y;
+    float travel_time;
+};
+
+struct Target
+{
+    Plank plank;
+    IntersectionPoint intersection;
+    int index;
+    float angle;
+};
 
 enum ai_State
 {
@@ -25,6 +40,12 @@ enum ai_State
     ai_tracking
 };
 
+struct ActionReward
+{
+    ai_State action;
+    float reward;
+};
+
 void createGrid(){
     float grid[22][22];
     for(int x = 0; x < 22; x++) {
@@ -32,7 +53,7 @@ void createGrid(){
             grid[x][y] = 0;
             if (x == 21 || x == 0 || y == 0) { grid[x][y] = -1000.0; }
             if (y == 21){
-                grid[x][y] = 10000.0;
+                grid[x][y] = 2000.0;
             }
         }
     }
@@ -126,23 +147,26 @@ Plank createPlank(float x, float y, float theta, int timeToTurn)
     plank.y_1 = check_ifInArena(timeToTurn*SPEED*sin(theta) + y);
     plank.x_2 = check_ifInArena((timeToTurn - 20)*SPEED*cos(theta) + x);
     plank.y_2 = check_ifInArena((timeToTurn - 20)*SPEED*sin(theta) + y);
+    
+    float dx = plank.x_2 - plank.x_1;
+    float dy = plank.y_2 - plank.y_1;
+    plank.length = sqrt(dx*dx + dy*dy);
+
     return plank;
 }
 
-float getPlankValue(float (*f)(float x, float y), float a_x, float a_y, float b_x, float b_y, int n){
-    float step_x = (a_x - b_x)/ n;  // width of each small rectangle
-    float step_y = (a_y - b_y)/ n;  // width of each small rectangle
+float getPlankValue(float (*f)(float x, float y), Plank plank, int n){
+
+    float step_x = (plank.x_1 - plank.x_2)/ n;  // width of each small rectangle
+    float step_y = (plank.y_1 - plank.y_2)/ n;  // width of each small rectangle
     float area = 0.0;  // signed area
 
-    float dx = b_x - a_x;
-    float dy = b_y - a_y;
-    float dist = sqrt(dx*dx + dy*dy);
-    std::cout << "Plank length" << dist << std::endl;
+    std::cout << "Plank length" << plank.length << std::endl;
 
     for (int i = 0; i < n; i++) {
-        area += f(a_x + (i + 0.5) * step_x, a_y + (i + 0.5) * step_y) * dist; // sum up each small rectangle
+        area += f(plank.x_1 + (i + 0.5) * step_x, plank.y_1 + (i + 0.5) * step_y) * plank.length; // sum up each small rectangle
     }
-    area = area/dist;
+    area = area/plank.length;
     return area;
 }
 
@@ -165,7 +189,7 @@ float findRobotValue(float x_robot, float y_robot, float theta, int timeToTurn)
         return 2*reward2;
     }
     else{
-        return abs(reward1-reward2);
+        return reward1-reward2;
     }
 
 }
@@ -192,23 +216,67 @@ bool targetIsMoving(int target, sim_Observed_State previous_state, sim_Observed_
     return moving;
 }
 
+double rotateX(double x, double y, double th) {
+    return x*cos(th)+y*sin(th);
+}
+
+double rotateY(double x, double y, double th) {
+    return x*-1*sin(th) + y*cos(th);
+}
+
+/*
+    A - ground robot
+    C - Drone
+*/
+double angleToIntercept(double Ax, double Ay, double Cx, double Cy, double Aangle, double Cvel) 
+{
+    double Avel = Robot_Speed/1000;
+    double rotationAngle = atan2(Ay-Cy, Ax-Cx) - M_PI/2;
+    double AxTrans = Ax-Cx;
+    double AyTrans = Ay-Cy;
+    double AxRot = rotateX(AxTrans, AyTrans, rotationAngle);
+    double AyRot = rotateY(AxTrans, AyTrans, rotationAngle);
+    double CxRot = 0; //rotateX(Cx, Cy, rotationAngle);
+    double CyRot = 0; //rotateY(Cx, Cy, rotationAngle);
+    
+    double AvelX = Avel*cos(Aangle);
+    double AvelY = Avel*sin(Aangle);
+    //double AvelXTrans = AvelX-Cx;
+    //double AvelYTrans = AvelY-Cy;
+    double AvelXrot = rotateX(AvelX, AvelY, rotationAngle);
+    double AvelYrot = rotateY(AvelX, AvelY, rotationAngle);
+
+    return acos((Avel/Cvel) * AvelXrot);
+}
+
+/*
+    A - ground robot
+    C - Drone
+*/
+float getTimeToIntercept(double Cx, double Ax, float Aangle, double Cvel, double angleToFly)
+{
+    double Avel = Robot_Speed/1000;
+    double AvelX = Avel*cos(Aangle);
+    return (float)(Cx-Ax)/(AvelX*Avel - cos(angleToFly)*Cvel);
+}
+
+/*
+    A - ground robot
+    C - Drone
+*/
+double getXPosIntercept(double droneX, double droneY, double flyAngle, double speed)
+{
+    return 0.0;
+}
+
+
 //Pseudo ish kode
-int choose_target(sim_Observed_State observed_state, sim_Observed_State previous_state){
+Target choose_target(sim_Observed_State observed_state, sim_Observed_State previous_state){
     int max_value = 0;
     int temp_value = 0;
-    int target = 0;
-    // for(int i = 0; i < Num_Targets; i++){
-    //     if(!state.target_removed[i]){
-    //         temp_value = findRobotValue(state.target_x[i], state.target_y[i],
-    //             state.target_q[i], (int)state.elapsed_time % 20);
+    int index = 0;
+    Target target;
 
-    //         if(temp_value < max_value){
-    //             max_value = temp_value;
-    //             target = i;
-    //         }
-    //     }
-
-    // }
     for(int i = 0; i < Num_Targets; i++){
         if(!observed_state.target_removed[i]){
             if (!targetIsMoving(i, previous_state, observed_state))
@@ -216,46 +284,124 @@ int choose_target(sim_Observed_State observed_state, sim_Observed_State previous
                 std::cout << "Target not moving" << std::endl;
                 continue;
             }
-            Plank positions = createPlank(observed_state.target_x[i], observed_state.target_y[i],
-            wrap_angle(observed_state.target_q[i]), (int)observed_state.elapsed_time % 20);
-            temp_value = getPlankValue(gridValue, positions.x_1, positions.y_1, positions.x_2, positions.y_2, 5);
+
+            float angle = wrap_angle(observed_state.target_q[i]);
+
+            Plank plank = createPlank(observed_state.target_x[i], observed_state.target_y[i],
+                angle, (int)observed_state.elapsed_time % 20);
+
+            temp_value = getPlankValue(gridValue, plank, 5);
             // temp_value = GRID[(int)observed_state.target_x[i]][(int)observed_state.target_y[i]];
 
             if(temp_value > max_value){
                 max_value = temp_value;
-                target = i;
+                target.index = i;
+                target.plank = plank;
+                target.angle = wrap_angle(observed_state.target_q[i]);
             }
         }
     }
     return target;
 }
 
-ai_State choose_action(sim_Observed_State state, int i){
-    int rewardOnTop = findRobotValue(state.target_x[i], state.target_y[i],
-            wrap_angle(state.target_q[i] + 0.785), (int)state.elapsed_time % 20); //0.785 radians is almost 45 degerees
+IntersectionPoint calculateInterceptionPoint(sim_Observed_State state, Target target){
+    IntersectionPoint intersection;
+    //calculate future x and y values to robot
+    //double distance = sqrt(pow(state.drone_x - state.target_x[i], 2) + pow(state.drone_y - state.target_y[i], 2));
+    //double time     = distance/
+    float drone_speed = Robot_Speed;
+    int i = target.index;
+    float angle = wrap_angle(state.target_q[i]);
+    double phi = angleToIntercept(state.target_x[i], state.target_y[i], state.drone_x, state.drone_y, angle, drone_speed);
+    intersection.travel_time = getTimeToIntercept(state.target_x[i], state.drone_x, state.target_q[i], drone_speed, phi);
+    
+    float distance = drone_speed*intersection.travel_time;
 
-    int rewardInFront = findRobotValue(state.target_x[i], state.target_y[i],
-            wrap_angle(state.target_q[i] + 3.14), (int)state.elapsed_time % 20);
+    intersection.x = state.drone_x + distance*cos(phi);
+    intersection.y = state.drone_y + distance*sin(phi);
 
-    int rewardForWait = findRobotValue(state.target_x[i], state.target_y[i],
-            state.target_q[i], (int)state.elapsed_time % 20);
+    return intersection;
+}
+
+ActionReward getBestActionAtPoint(Target target, sim_Observed_State state) {
+    ActionReward action_reward;
+    int rewardOnTop = findRobotValue(target.intersection.x, target.intersection.y,
+            wrap_angle(target.angle + 0.785), (int)(state.elapsed_time+target.intersection.travel_time) % 20); //0.785 radians is almost 45 degerees
+
+    int rewardInFront = findRobotValue(target.intersection.x, target.intersection.y,
+            wrap_angle(target.angle + 3.14), (int)(state.elapsed_time+target.intersection.travel_time) % 20);
+
+    int rewardForWait = findRobotValue(target.intersection.x, target.intersection.y,
+            target.angle, (int)(state.elapsed_time+target.intersection.travel_time) % 20);
     std::cout << "Reward in front " << rewardInFront << std::endl;
     std::cout << "Reward on top " << rewardOnTop << std::endl;
     std::cout << "Reward wait" << rewardForWait << std::endl;
 
     int max_reward = std::max(std::max(rewardInFront,rewardOnTop), rewardForWait);
     if(max_reward == rewardForWait){
-        return ai_waiting;
+        action_reward.action = ai_waiting;
+        action_reward.reward = rewardForWait;
     }
     else if(max_reward == rewardInFront){
-        return ai_landingInFront;
+        action_reward.action = ai_landingInFront;
+        action_reward.reward = rewardInFront;
     }
     else if(max_reward == rewardOnTop){
-        return ai_landingOnTop;
+        action_reward.action = ai_landingOnTop;
+        action_reward.reward = rewardOnTop;
     }
     else{
-        return ai_waiting;
+        action_reward.action = ai_waiting;
+        action_reward.reward = rewardForWait;
     }
+    return action_reward;
+}
+
+ai_State choose_action(sim_Observed_State state, Target target){
+    int index = target.index;
+    float angle = wrap_angle(state.target_q[index]);
+
+    target.intersection = calculateInterceptionPoint(state, target);
+
+
+    float n = 10;
+    float step_size = target.plank.length/n;
+    float step_x = step_size*cos(angle);
+    float step_y = step_size*sin(angle);
+
+    float x = target.intersection.x;
+    float y = target.intersection.y;
+    int time_to_turn = ((int)state.elapsed_time % 20) - target.intersection.travel_time;
+
+    // Temporary max rewarded action
+    ActionReward best_action;
+    best_action.reward = -1000.0;
+    best_action.action = ai_waiting;
+    ActionReward action_to_check;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (time_to_turn <= 0) {
+            std::cout << "Time ran out before end of plank was reached " << std::endl;
+            break;
+        }
+        if (x == target.plank.x_1 && y == target.plank.y_1) {
+            std::cout << "End of plank was reached ";
+            break;
+        }
+
+        action_to_check = getBestActionAtPoint(target, state);
+
+        if (action_to_check.reward > best_action.reward) {
+            best_action = action_to_check;
+        }
+
+        x = x+step_x;
+        y = y+step_y;
+        time_to_turn = time_to_turn - Robot_Speed/(step_size*1000); // Multiplied by 1000 to get Milimeters from Meters
+    }
+
+    return best_action.action;
 }
 
 int main()
@@ -272,8 +418,11 @@ int main()
     sim_Command cmd;
     sim_Observed_State observed_state;
     sim_Observed_State previous_state;
-    int target = -1;
+    int target_index = -1;
     float last_action_time;
+
+    Target target;
+
     while (running)
     {
         sim_recv_state(&state);
@@ -284,29 +433,30 @@ int main()
         {
             last_action_time = observed_state.elapsed_time;
 
-            if(target == -1){
+            if(target_index == -1){
                 target = choose_target(observed_state, previous_state);
+                target_index = target.index;
                 ai_state = ai_tracking;
                 cmd.type = sim_CommandType_Track;
-                cmd.i = target;
+                cmd.i = target.index;
                 std::cout << "Tracking" << std::endl;
             }
 
-            else if(target_inActionRange(observed_state, target)
-                    && targetIsMoving(target, previous_state, observed_state))
-            {
+            else if(target_inActionRange(observed_state, target.index)
+                    && targetIsMoving(target.index, previous_state, observed_state))
+            {                
                 ai_state = choose_action(observed_state, target);
                 switch (ai_state)
                 {
                     case ai_landingOnTop:
                         cmd.type = sim_CommandType_LandOnTopOf;
-                        cmd.i = target;
+                        cmd.i = target.index;
                         std::cout << "Top" << std::endl;
                         sim_send_cmd(&cmd);
                     break;
                     case ai_landingInFront:
                         cmd.type = sim_CommandType_LandInFrontOf;
-                        cmd.i = target;
+                        cmd.i = target.index;
                         std::cout << "Front" << std::endl;
                         sim_send_cmd(&cmd);
                     break;
@@ -314,7 +464,7 @@ int main()
                         std::cout << "WAITING" << std::endl;
                     break;
                 }
-            target = -1;
+            target_index = -1;
             }
         }
     }
